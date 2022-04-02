9439ac2e6febb5dd7424fe4ebe1f16e85d85cf1b
kuruthi <venkateshwarp81@gmail.com>
2022-04-03 00:48:42 +0530

    Added comments and formatting to the R script

--- t3db/toxin.R
+++ t3db/toxin.R
@@ -1,14 +1,26 @@
-library(jsonlite)
+#Author: P. Venkateshwar
 
-setwd("~/Documents/toxin_project/t3db/")
+#Description
+#An R script that outputs the toxin names from the data downloaded 
+#+from the t3db database
 
-t3db <- fromJSON("toxins.json",simplifyDataFrame = TRUE)
-View(t3db)
-t3db$uniprot_id
-pubchem <- t3db$pubchem_id
-t3db$id[pubchem_id='104737']
-class(pubchem)
-as.numeric(pubchem)
+library(jsonlite) # loading the jsonlite library
+setwd("~/bacterial-flagella/t3db/") # Path to the file directory
+
+t3db <- fromJSON("toxins.json",simplifyDataFrame = TRUE) # Reading the json file from t3db database onto the R environment
+
+#View(t3db) # Uncomment to view in the R environment
+
+#t3db$uniprot_id # The database is not complete and the proteins do not have a matching uniprot ID entry
+
+#pubchem <- t3db$pubchem_id
+
+#t3db$id[pubchem_id='104737']
+
+# Loading the downloaded csv file into the R environment
 protein_toxins <- read.csv("protein_toxins.csv")
-View(protein_toxins)
+
+#View(protein_toxins) #uncomment to view in the R environment
+
+#Write the toxin names to a txt file, edit the name if needed
 write(protein_toxins$Toxin.name,"toxin_names.txt")
