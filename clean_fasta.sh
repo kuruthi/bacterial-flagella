a5a99ab29c57c4aa3c86e68edb02b10bea068746
kuruthi <venkateshwarp81@gmail.com>
2022-04-07 19:05:06 +0530

    Added a simple script that formats fasta output

---
+++ clean_fasta.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Author: P. Venkateshwar
+#Date created: 07-04-2022
+#Last modified:
+
+#Description
+#Script takes a fasta file as input and removes unnecessary line carriages in the sequences
+
+set -e
+set -x
+
+[[ -f $1 ]] && input="$1"
+
+cat "$input" | tr -d "\n" | sed -e 's/]/]\n/g' -e 's/>/\n>/g' -e 's/\n>/>/1' > "$input".new
+#Works but need to add checks to make sure there are no descrepancies between the number of seqs in the input and the output
+echo "Done"
+
+exit
