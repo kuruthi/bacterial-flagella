d9e398ea85a76afaa74bc4e586057e045870bd19
kuruthi <venkateshwarp81@gmail.com>
2022-04-13 19:08:55 +0530

    removed redundant files, added more comments

--- scripts/entrez_v2.sh
+++ scripts/entrez_v2.sh
@@ -11,15 +11,20 @@
 #+number of results is lesser.
 #This will hopefully solve the problem of redundant sequences getting downloaded.
 
+#Usage
+#Check whether the script and the input file are in your current working directory
+#Else you may move the script to your PATH and avoid using './'
+#./entrez_v2.sh [INPUT FILE AS FASTA] [OUTPUT FILE NAME] [OUTPUT FORMAT]
+#The third argument is not mandatory, if not supplied the script returns the output as a fasta file
 set -e #Exit in case of problems
 #set -x #Only for debugging #Comment or uncomment
 
 clear #clears the terminal clutter
 
-source ~/bacterial-flagella/scripts/functions.sh
+source ~/bacterial-flagella/scripts/functions.sh ## Sourcing the script file with my functions,
 
 check_args "$1" "$2" "$3" ##Calling the function
 
-prot_result_count ##
+prot_result_count ## Calling the second function
 
 exit
