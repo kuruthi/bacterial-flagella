67f28440b571274142bd25d09ade67d6e21784dd
kuruthi <venkateshwarp81@gmail.com>
2022-05-01 15:20:13 +0530

    expanded functionality for clean_fasta.sh, now counts the size of the sequence, need to work on it more

--- clean_fasta.sh
+++ clean_fasta.sh
@@ -2,17 +2,32 @@
 
 #Author: P. Venkateshwar
 #Date created: 07-04-2022
-#Last modified:
+#Last modified: 01-05-2022
 
 #Description
 #Script takes a fasta file as input and removes unnecessary line carriages in the sequences
-
+#Adding functionalities
 set -e
-set -x
+#set -x
 
 [[ -f $1 ]] && input="$1"
 
-cat "$input" | tr -d "\n" | sed -e 's/]/]\n/g' -e 's/>/\n>/g' -e 's/\n>/>/1' > "$input".new
+tr -d "\n" < "$input" | sed -e 's/]/]\n/g' -e 's/>/\n>/g' -e 's/\n>/>/1' > "$input".temp
+readarray -t fasta_array < "$input".temp
+
+for index in "${!fasta_array[@]}"; ## Don't know the use of "!" here, but the script needs it
+do
+	if [[ $(( index % 2 )) != 0 ]] 
+	then
+	declare "$( echo "${fasta_array[ (( index - 1 )) ]}" | cut -f 1 -d " " | tr -d ">_." )"="${#fasta_array[index]}"
+	echo "$"$(echo "${fasta_array[ (( index - 1 )) ]}" | cut -f 1 -d " " | tr -d ">_." )""
+
+	fi
+	#i=$(( i + 1 )) #incrementing
+done
+
+#echo "${#fasta_array[@]}"
+#echo "${#fasta_array[1]}"
 #Works but need to add checks to make sure there are no descrepancies between the number of seqs in the input and the output
 echo "Done"
 
